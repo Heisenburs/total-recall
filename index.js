9dec2d2a2d426d69dc66129f9f0262e73e823762
Heisenburs <89811609+Heisenburs@users.noreply.github.com>
2023-02-14 10:55:51 -0500

    Exercise 1 & 2 finished

--- index.js
+++ index.js
@@ -8,9 +8,39 @@
 */
 
 // I. Variables & Data Types
+
 // A. Q + A
+
+/*
+1. using " = " , the assignment operator
+2. use the assignment operator and define a new value
+3. create a new variable, with a new name, and set it equal to the existing variable using an assignment operator
+4 declare: set name for a variable
+assign: giving a variable a value
+define: declaring & assigning at the same time
+5. Explaining the code steps in plain English so that you can follow along step by step and convert it into syntax, helps create a blueprint for the code
+6. 50/50
+*/
+
 // B. Strings
+
+let firstVariable = "Hello World";
+
+firstVariable = 32;
+console.log(firstVariable);
+
+let secondVariable = firstVariable;
+
+secondVariable = "cow";
+console.log(secondVariable);
+
+//32
+
+let yourName = "Jada";
+console.log(`Hello, my name is ${yourName}`);
+
 // C. Booleans
+
 // D. The Farm
 // E. Driver's Ed
 //_____________________________________________________________
