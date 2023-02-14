08074de228d717b94a597a594bee754183c43850
Heisenburs <89811609+Heisenburs@users.noreply.github.com>
2023-02-14 11:18:00 -0500

    The Farm ex. completed

--- index.js
+++ index.js
@@ -40,24 +40,34 @@ define: declaring & assigning at the same time
 // console.log(`Hello, my name is ${yourName}`);
 
 // C. Booleans
-const a = 4;
-const b = 53;
-const c = 57;
-const d = 16;
-const e = "Kevin";
 
-console.log(a < b);
-console.log(c > d);
-console.log("Name" === "Name");
-console.log(true || false);
-console.log((false && false && false && false && false) || true);
-console.log(false === false);
-console.log(e === "Kevin");
-console.log(a + b === c); // note: a < b < c is NOT CORRECT (and is not a valid JS expression, think about using other math operations)
-console.log(a * a === d); // note: the answer is a simple arithmetic equation, not something "weird"
-console.log(48 !== "48");
+// const a = 4;
+// const b = 53;
+// const c = 57;
+// const d = 16;
+// const e = "Kevin";
+
+// console.log(a < b);
+// console.log(c > d);
+// console.log("Name" === "Name");
+// console.log(true || false);
+// console.log((false && false && false && false && false) || true);
+// console.log(false === false);
+// console.log(e === "Kevin");
+// console.log(a + b === c); // note: a < b < c is NOT CORRECT (and is not a valid JS expression, think about using other math operations)
+// console.log(a * a === d); // note: the answer is a simple arithmetic equation, not something "weird"
+// console.log(48 !== "48");
 
 // D. The Farm
+
+let animal = "cow";
+
+if (animal === "cow") {
+  console.log(`moooooo`);
+} else {
+  console.log(`Hey! You're not a cow`);
+}
+
 // E. Driver's Ed
 //_____________________________________________________________
 
