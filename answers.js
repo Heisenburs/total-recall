62402b043a1a451c7afeafde5d5d0cba9445e3b6
Heisenburs <89811609+Heisenburs@users.noreply.github.com>
2023-02-15 15:11:31 -0500

    Wild Wild Life answered

--- answers.js
+++ answers.js
@@ -19,16 +19,42 @@ for (i = 0; i <= 200; i++) {
 
 for (i = 1; i <= 100; i++) {
   if (i % 5 === 0 && i % 3 === 0) {
-    // can't get FizzBuzz
-    console.log(` ${i} FizzBuzz`);
+    // console.log(` ${i} FizzBuzz`);
   } else if (i % 5 === 0) {
-    console.log(`${i} Buzz`);
+    // console.log(`${i} Buzz`);
   } else if (i % 3 === 0) {
-    console.log(`${i} Fizz`);
+    // console.log(`${i} Fizz`);
   }
 }
 
 //* WILD WILD LIFE \\
+
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"];
+const sharky = ["Sharky", "shark", 20, "Left Coast"];
+const plantee = ["Plantee", "plant", 5000, "Mordor"];
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"];
+const dart = ["D'Art", "Demogorgan Dog", 2, "Upside Down"];
+
+// 1
+
+plantee[2] = 5001;
+console.log(plantee);
+
+// 2
+
+wolfy[3] = "Gotham City";
+console.log(wolfy);
+
+// 3 Give D'Art a second hometown by adding "Hawkins"
+
+dart.push("Hawkins");
+console.log(dart);
+
+// 4 Remove "Wolfy" from the wolfy array and replace it with "Gameboy".
+
+wolfy.splice(0, 1, "Gameboy");
+console.log(wolfy);
+
 //* YELL AT THE NINJA TURTLES \\
 //* METHODS, REVISITED \\
 //* WHERE IS WALDO \\
