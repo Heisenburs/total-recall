f21e3b910f54a44b858a5fb37e45efeb36ba966f
Heisenburs <89811609+Heisenburs@users.noreply.github.com>
2023-02-15 13:15:48 -0500

    Easy Going answered

--- answers.js
+++ answers.js
@@ -3,11 +3,15 @@
 
 //* EASY GOING \\
 
+for (i = 1; i <= 20; i++) {
+  console.log(i);
+}
+
 //* GET EVEN \\
 
 for (i = 0; i <= 200; i++) {
   if (i % 2 === 0) {
-    console.log(i);
+    // console.log(i);
   }
 }
 
