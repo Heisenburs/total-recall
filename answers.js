10155187615b3ee9c63f1ceb642306df3e28af86
Heisenburs <89811609+Heisenburs@users.noreply.github.com>
2023-02-15 13:13:54 -0500

    Get Even answered

--- answers.js
+++ answers.js
@@ -1,5 +1,16 @@
+//* PART I: HTML & CSS
+// 4
+
 //* EASY GOING \\
+
 //* GET EVEN \\
+
+for (i = 0; i <= 200; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
+
 //* FIZZ BUZZ \\
 //* WILD WILD LIFE \\
 //* YELL AT THE NINJA TURTLES \\
